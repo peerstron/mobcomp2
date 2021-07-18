beae2d31924d4ce2bf4e8ed038933fa2a758f3f9
Nyarko Ebenezer <blissful2999@gmail.com>
2021-07-18 23:42:37 +0000

    setups and first work

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Import Screens
+import LoginScreen from './src/screens/LoginScreen'
+import OverviewScreen from "./src/screens/OverviewScreen";
+import PaymentScreen from "./src/screens/PaymentScreen";
+
+
+export default function App() {
+  return (
+    <LoginScreen />
+    // <OverviewScreen />
+    // <PaymentScreen />
+  );
+}
